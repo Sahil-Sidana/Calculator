d53cf4cbe66d87d854cc28ef9282fbdaa3e42809
Sahil Sidana <sahilsidana2002@gmail.com>
2023-12-19 18:19:57 +0530

    almostdone

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const buttons=document.querySelectorAll('button');
+const screen=document.getElementById('screen');
 
 for(let button of buttons){
 
     button.addEventListener('click',(e)=>{
         const buttonText = e.target.innerText;
         console.log(buttonText);
+        if(buttonText==='C') screen.value="";
+        else if(buttonText==='^') screen.value+='**';
+        else if(buttonText==='='){
+
+            try{
+                screen.value=(eval(screen.value));
+            }
+            catch(e){
+                screen.value="Invalid Operation";
+                console.log(e);
+            }    
+        }
+        else screen.value+=buttonText;
     });
 }
